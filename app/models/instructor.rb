3e62a14f0d8a4217828b3dd09861b537c439d674
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-13 19:36:03 -0400

    commit tree 44ea73421e24172b8372c7b8397e9d357904869c

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,17 @@
 class Instructor
+    attr_reader :student, :name, :status, :instructor
 
+    @@all = []
+
+    def initialize(student, name, status, instructor)
+        @student = student
+        @name = name
+        @status = status
+        @instructor = instructor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,35 @@
 class Student
+    attr_reader :first_name
+
+    @@all = []
+
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+
+    def self.all 
+        @@all
+    end
+
+    def add_boating_test(boating_test_name, boating_test_status, instructor)
+        BoatingTest.new(self, boating_test_name, boating_test_status, instructor)
+    end
+
+    def self.find_student(name)
+        self.all.find{ |student| student.name == name}
+    end
+
+    def grade_percentage
+        passed_tests = BoatingTest.all.select { |test| test.status == "passed"}
+
+        (passed_tests.length.to_f / BoatingTest.all.length) * 100
+    end
+
+
+
+
 
 end
