0e58b1c3d9ada6086c833b14fc6ddeaf496ebe2c
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-13 20:44:36 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,48 @@
 class Instructor
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def passed_students
+        students_tests = BoatingTest.all.select { |test| test.instructor == self }
+
+        students_tests.select { |test| test.status == "passed"}
+    end
+
+    def pass_student(student, test_name)
+
+        test_found = BoatingTest.all.find { |test| test.student.first_name == student.first_name && test.name == test_name}
+
+        if test_found
+            test_found.status = "passed"
+        else
+            BoatingTest.new(student, test_name, "passed", self)
+        end
+    end
+
+    def fail_student(student, test_name)
+        test_found = BoatingTest.all.find { |test| test.student.first_name == student.first_name && test.name == test_name}
+
+        if test_found
+            test_found.status = "failed"
+        else
+            BoatingTest.new(student, test_name, "failed", self)
+        end
+    end
+
+    def all_students
+        students_tests = BoatingTest.all.select { |test| test.instructor == self }
+
+        students_tests.map { |test| test.student}
+        
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,35 @@
 class Student
+    attr_reader :first_name
+
+    @@all = []
+
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+
+    def self.all 
+        @@all
+    end
+
+    def add_boating_test(boating_test_name, boating_test_status, instructor)
+        BoatingTest.new(self, boating_test_name, boating_test_status, instructor)
+    end
+
+    def self.find_student(name)
+        self.all.find{ |student| student.first_name == name }
+    end
+
+    def grade_percentage
+        student_tests = BoatingTest.all.select { |test| test.student == self }
+
+        passed_tests = student_tests.select { |test| test.status == "passed" }
+
+        (passed_tests.length.to_f / student_tests.length) * 100
+    end
+
+    
 
 end
